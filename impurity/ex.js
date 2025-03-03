fc267bdb2105518471100beb4f1ad8caa47430dd
Eulier González <eulier1@gmail.com>
2025-03-03 11:58:39 -0300

    wrapping a function to contain impurity

--- impurity/ex.js
+++ impurity/ex.js
@@ -8,14 +8,20 @@ var students = [
 	{ id: 491, name: "Ally" }
 ];
 
-function sortStudentsByName() {
-	// Don't modify this function
-	students.sort(function byName(s1,s2){
-		if (s1.name < s2.name) return -1;
-		else if (s1.name > s2.name) return 1;
-		else return 0;
-	});
-	return students;
+function getStudentsByName(students) { 
+	students = [...students]
+
+	return sortStudentsByName()
+
+	function sortStudentsByName() {
+		// Don't modify this function
+		students.sort(function byName(s1,s2){
+			if (s1.name < s2.name) return -1;
+			else if (s1.name > s2.name) return 1;
+			else return 0;
+		});
+		return students;
+	}
 }
 
 function sortStudentsByID() {
@@ -29,7 +35,7 @@ function sortStudentsByID() {
 // *************************************
 
 // modify/move this function
-function getStudentsByName() { return students; }
+
 
 // modify/move this function
 function getStudentsByID() { return students; }
