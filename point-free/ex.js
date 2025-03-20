d4d3e4fd18e2e4fe210fc6ecffca4cf87a6d6869
Eulier González <eulier1@gmail.com>
2025-03-20 18:44:25 -0300

    transform into a point-free style

--- point-free/ex.js
+++ point-free/ex.js
@@ -1,24 +1,45 @@
 "use strict";
 
-function output(txt) {
-	console.log(txt);
+// function output(txt) {
+// 	console.log(txt);
+// }
+
+const output = console.log.bind(this)
+
+// function printIf(shouldPrintIt) {
+// 	return function(msg) {
+// 		if (shouldPrintIt(msg)) {
+// 			output(msg);
+// 		}
+// 	};
+// }
+
+function when(fn) {
+	return function(predicate) {
+		return function(...args) {
+			if(predicate(...args))
+				return fn(...args)
+		}
+	}
 }
 
-function printIf(shouldPrintIt) {
-	return function(msg) {
-		if (shouldPrintIt(msg)) {
-			output(msg);
-		}
-	};
-}
+const printIf = when(output)
 
 function isShortEnough(str) {
 	return str.length <= 5;
 }
 
-function isLongEnough(str) {
-	return !isShortEnough(str);
+// function isLongEnough(str) {
+// 	return !isShortEnough(str);
+// }
+
+function not(fn) {
+	return function(...args) {
+		return !fn(...args)
+	}
 }
+
+const isLongEnough = not(isShortEnough)
 
 var msg1 = "Hello";
 var msg2 = msg1 + " World";
